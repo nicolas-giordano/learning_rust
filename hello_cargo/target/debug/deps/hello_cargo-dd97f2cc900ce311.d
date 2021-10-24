236e5528feed60c1f857896b5532131968483b5e
Nicolas Giordano <nickgior@gmail.com>
2021-10-24 14:29:04 -0400

    Created a project using cargo. Learned cargo build, cargo run, and cargo check.

---
+++ hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311.d
@@ -0,0 +1,5 @@
+/Users/nick/Documents/learning_rust/hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311: src/main.rs
+
+/Users/nick/Documents/learning_rust/hello_cargo/target/debug/deps/hello_cargo-dd97f2cc900ce311.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/nick/Documents/learning_rust/hello_cargo/target/debug/hello_cargo: /Users/nick/Documents/learning_rust/hello_cargo/src/main.rs
